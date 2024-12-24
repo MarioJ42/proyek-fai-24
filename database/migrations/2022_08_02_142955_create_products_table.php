a47f141f9837dabc002d7411785d9981520e6f27
Stevanus Hia <134584953+MarioJ42@users.noreply.github.com>
2024-12-24 09:28:54 +0700

    minor fix seeder and migration #17

--- database/migrations/2022_08_02_142955_create_products_table.php
+++ database/migrations/2022_08_02_142955_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer('id_supplier');
             $table->string('product_name', 25)->unique();
             $table->text('orientation');
             $table->text('description');
