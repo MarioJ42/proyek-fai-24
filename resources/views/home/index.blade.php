316f8c18eb21ce79c300fb0453c87b22e72fc4d8
Mario Joseph <mariojoseph3939@gmail.com>
2025-01-08 23:56:11 +0700

    bug fix datatables #21

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -32,14 +32,6 @@
     @push('scripts-dependencies')
         <script src="/js/sales_chart.js"></script>
         <script src="/js/profits_chart.js"></script>
-        <!-- Include DataTables JS -->
-        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#suppliersTable').DataTable();
-            });
-        </script>
     @endpush
 @endcan
 
--- resources/views/partials/admin/supplier_list.blade.php
+++ resources/views/partials/admin/supplier_list.blade.php
@@ -1,45 +1,59 @@
 @extends('layouts.main')
 
+@push('css-dependencies')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" />
+@endpush
+
+@push('scripts-dependencies')
+    <script src="/js/customers_table.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+@endpush
+
 @section('content')
 @php
     $title = "Supplier List "
 @endphp
-<div class="container mt-4">
-    <h2 class="mb-4">List Supplier</h2>
+<div class="container-fluid mt-4 px-3">
 
-    <table id="suppliersTable" class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nama Supplier</th>
-                <th>Email</th>
-                <th>Telepon</th>
-                <th>Alamat</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($suppliers as $supplier)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $supplier->fullname }}</td>
-                <td>{{ $supplier->email }}</td>
-                <td>{{ $supplier->phone }}</td>
-                <td>{{ $supplier->address }}</td>
-                <td>
-                    <a href="" class="btn btn-primary btn-sm">
-                        <i class="fa-solid fa-cart-shopping"></i> Lihat Produk
-                    </a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @include('/partials/breadcumb')
+
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-fw fa-solid fa-users me-1"></i>
+            Customers
+        </div>
+        <div class="card-body">
+            <table id="datatablesSimple">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nama Supplier</th>
+                        <th>Email</th>
+                        <th>Telepon</th>
+                        <th>Alamat</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($suppliers as $supplier)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $supplier->fullname }}</td>
+                        <td>{{ $supplier->email }}</td>
+                        <td>{{ $supplier->phone }}</td>
+                        <td>{{ $supplier->address }}</td>
+                        <td>
+                            <a href="" class="btn btn-primary btn-sm">
+                                <i class="fa-solid fa-cart-shopping"></i> Lihat Produk
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
-
-@push('styles')
-
-@endpush
 
 {{-- {{ route('admin.supplier.products', $supplier->id) }} --}}
