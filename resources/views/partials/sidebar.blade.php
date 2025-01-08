d31cda697b27df8aa4312631cd095384cf3707d8
JoshuaNathan1 <135298681+JoshuaNathan1@users.noreply.github.com>
2025-01-08 20:47:13 +0700

    Test Final 1.0

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -16,7 +16,7 @@
                     Transaksi
                 </a>
 
-            {{-- @elsecan("is_supplier")
+            @elsecan("is_supplier")
                 <div class="sb-sidenav-menu-heading">Supplier</div>
                 <a class="nav-link" href="/home/suppliers">
                     <div class="sb-nav-link-icon"><i class="fas fa-fw fa-home-alt"></i></i></div>
@@ -29,7 +29,7 @@
                 <a class="nav-link" href="/product">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-clock-rotate-left"></i></div>
                     History
-                </a> --}}
+                </a>
             @else
                 <div class="sb-sidenav-menu-heading">Customer</div>
                 <a class="nav-link" href="/home">
@@ -40,24 +40,24 @@
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-fw fa-paw"></i></div>
                     My Point
                 </a>
-                <div class="sb-sidenav-menu-heading">Interface</div>
-                <a class="nav-link" href="/product">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-fw fa-dumpster"></i></div>
-                    Product
-                </a>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
-                aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-fw fa-columns"></i></div>
-                    Order
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-fw fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="/order/order_data">Order Data</a>
-                        <a class="nav-link" href="/order/order_history">Order History</a>
-                    </nav>
-                </div>
             @endcan
+            <div class="sb-sidenav-menu-heading">Interface</div>
+            <a class="nav-link" href="/product">
+                <div class="sb-nav-link-icon"><i class="fa-solid fa-fw fa-dumpster"></i></div>
+                Product
+            </a>
+            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
+            aria-expanded="false" aria-controls="collapseLayouts">
+                <div class="sb-nav-link-icon"><i class="fas fa-fw fa-columns"></i></div>
+                Order
+                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-fw fa-angle-down"></i></div>
+            </a>
+            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <nav class="sb-sidenav-menu-nested nav">
+                    <a class="nav-link" href="/order/order_data">Order Data</a>
+                    <a class="nav-link" href="/order/order_history">Order History</a>
+                </nav>
+            </div>
         </div>
     </div>
     <div class="sb-sidenav-footer">
