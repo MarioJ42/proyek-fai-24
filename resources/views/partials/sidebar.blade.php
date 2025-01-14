4cb5ab7f8009d571d6cb428c96d26c2bd675aae3
JoshuaNathan1 <135298681+JoshuaNathan1@users.noreply.github.com>
2025-01-14 20:14:57 +0700

    Penambahan Interface di Menu admin 2.2

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,11 +3,32 @@
         <div class="nav">
             @can("is_admin")
                 <div class="sb-sidenav-menu-heading">Administrator</div>
-                <div class="sb-sidenav-menu-heading">Customer</div>
                 <a class="nav-link" href="/home">
                     <div class="sb-nav-link-icon"><i class="fas fa-fw fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
+                <div class="sb-sidenav-menu-heading">Interface</div>
+                <a class="nav-link" href="/transaction">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-fw fa-dollar-sign"></i></div>
+                    History Admin
+                </a>
+                <a class="nav-link" href="/product">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-fw fa-dumpster"></i></div>
+                    Add Product
+                </a>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
+                aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-fw fa-columns"></i></div>
+                    Order
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-fw fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="/order/order_data">Order Data</a>
+                        <a class="nav-link" href="/order/order_history">Order History</a>
+                    </nav>
+                </div>
+                <div class="sb-sidenav-menu-heading">Customer</div>
                 <a class="nav-link" href="/home/customers">
                     <div class="sb-nav-link-icon"><i class="fas fa-fw fa-solid fa-users"></i></div>
                     Customers
@@ -25,23 +46,7 @@
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-user-plus"></i></div>
                     Add Suppliers
                 </a>
-                <div class="sb-sidenav-menu-heading">Interface</div>
-                <a class="nav-link" href="/transaction">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-fw fa-dollar-sign"></i></div>
-                    History Admin
-                </a>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
-                aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-fw fa-columns"></i></div>
-                    Order
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-fw fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="/order/order_data">Order Data</a>
-                        <a class="nav-link" href="/order/order_history">Order History</a>
-                    </nav>
-                </div>
+                
 
             @elsecan("is_supplier")
                 <div class="sb-sidenav-menu-heading">Supplier</div>
